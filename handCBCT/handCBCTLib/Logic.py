d9115890baf7cc04510ec3d55af894c7b8530010
samuel.yu <samuelyu191@gmail.com>
2026-01-12 16:41:18 -0700

    modified: UI changes, changed process method to reflect changes in parameter nodes. WIP on running nnUNet model

--- handCBCT/handCBCTLib/Logic.py
+++ handCBCT/handCBCTLib/Logic.py
@@ -31,20 +31,20 @@ class handCBCTLogic(ScriptedLoadableModuleLogic):
         """
         Initialize parameter node with default settings.
         """
+        
+        """ Code provided by Extension Wizard
         if not parameterNode.GetParameter("Threshold"):
             parameterNode.SetParameter("Threshold", "100.0")
         if not parameterNode.GetParameter("Invert"):
             parameterNode.SetParameter("Invert", "false")
-
-    def process(self, inputVolume, outputVolume, imageThreshold, invert=False, showResult=True):
+        """
+    def process(self, inputVolume, outputVolume):
         """
         Run the processing algorithm.
         Can be used without GUI widget.
-        :param inputVolume: volume to be thresholded
-        :param outputVolume: thresholding result
-        :param imageThreshold: values above/below this threshold will be set to 0
-        :param invert: if True then values above the threshold will be set to 0, otherwise values below are set to 0
-        :param showResult: show output volume in slice viewers
+        :param inputVolume: volume to be segmented (nii expected)
+        :param outputVolume: segmentation result
+
         """
         if not self.is_setup:
           self.setup()
@@ -55,19 +55,10 @@ class handCBCTLogic(ScriptedLoadableModuleLogic):
         import time
         startTime = time.time()
         logging.info('Processing started')
-
-        # Compute the thresholded output volume using the "Threshold Scalar Volume" CLI module
-        cliParams = {
-            'InputVolume': inputVolume.GetID(),
-            'OutputVolume': outputVolume.GetID(),
-            'ThresholdValue': imageThreshold,
-            'ThresholdType': 'Above' if invert else 'Below'
-        }
-        cliNode = slicer.cli.run(slicer.modules.thresholdscalarvolume, None, cliParams, wait_for_completion=True, update_display=showResult)
-        # We don't need the CLI module node anymore, remove it to not clutter the scene with it
-        slicer.mrmlScene.RemoveNode(cliNode)
-
-        stopTime = time.time()
+        
+        # TODO: enable modification of parameters, specifically the fold count
+        self.segmentationLogic.startSegmentation(inputVolume)
+        
         logging.info(f'Processing completed in {stopTime-startTime:.2f} seconds')
 
 
@@ -101,7 +92,10 @@ class handCBCTLogic(ScriptedLoadableModuleLogic):
       from SlicerNNUNetLib import SegmentationLogic
 
       self.segmentationLogic = SegmentationLogic()
-
+      self.segmentationLogic.progressInfo.connect(print)
+      self.segmentationLogic.errorOccurred.connect(slicer.util.errorDisplay)
+      self.segmentationLogic.inferencedFinished.connect(self.segmentationLogic.loadSegmentation)
+      
       self.loadWeights()
       self.is_setup = True
 
@@ -123,7 +117,6 @@ class handCBCTLogic(ScriptedLoadableModuleLogic):
         slicer.util.messageBox("Model directory is not valid.")
 
 
-      self.segmentationLogic = SegmentationLogic()
       self.segmentationLogic.setParameter(self.modelParameters)
 
 
