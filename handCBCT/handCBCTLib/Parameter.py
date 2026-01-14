5188fba57d1bb472ea3ba7222f011d8f078dc4cd
SaNi191 <samuelyu191@gmail.com>
2026-01-14 16:09:32 -0700

    added: parameter node wrapper class

---
+++ handCBCT/handCBCTLib/Parameter.py
@@ -0,0 +1,28 @@
+"""
+Parameter Node wrapper class for handCBCT
+
+Could be merged into Logic.py, but would not make much sense semantically
+"""
+
+from typing import Annotated
+
+from slicer import vtkMRMLScalarVolumeNode, vtkMRMLSegmentationNode
+from slicer.parameterNodeWrapper import parameterNodeWrapper, Choice
+
+
+@parameterNodeWrapper
+class handCBCTParameterNode:
+    """
+    Parameters for handCBCT module
+
+    inputVolume - volume to segment
+    foldCount - number of folds for nnUNet model configuration
+    deviceType - the device type for nnUNet model configuration
+    outputSegment - segmentation result
+    """
+
+    inputVolume: vtkMRMLScalarVolumeNode
+    foldCount: Annotated[int, Choice[1, 2, 3, 4, 5]] = 1 # default to 1 for performance purposes
+    deviceType: Annotated[str, Choice["cuda", "gpu", "mps"]] = "cuda"
+    outputSegment: vtkMRMLSegmentationNode
+
